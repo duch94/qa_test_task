1d7e08382acc0817cfa84e2f723dbcf4ce897374
duch94 <vtduch@gmail.com>
2018-05-11 19:16:17 +0300

    uploaded task 3

--- src/main/java/Task2.java
+++ src/main/java/Task2.java
@@ -1,5 +1,12 @@
+import java.util.List;
+
 public class Task2 {
+
     public static void main(String[] args) {
-        System.out.println("kappa keepo");
+        RunMethods methodRunner = new RunMethods();
+        String testsPath = "conf/jobs.yml";
+        List<String> tests = methodRunner.getTests(testsPath);
+        LkTest testsObject = new LkTest();
+        methodRunner.runTestMethods(testsObject, tests);
     }
 }
